7034b1680797541ceda82025c87a92c8ead4a9f6
caoxilong <2905405962@qq.com>
2023-03-17 13:17:08 +0800

    2023/3/13

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <header class="header">header</header>
+    <div class="content">content</div>
+    <footer class="footer">footer</footer>
+  </div>
+  <!--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
+  <div class="landscape-tip">请将屏幕竖向浏览</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss">
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .header {
+    // height: 44px;
+    height: 0.44rem;
+    background-color: #f66;
+  }
+  .content {
+    flex: 1;
+    overflow: auto;
+  }
+  .footer {
+    // height: 50px;
+    height: 0.5rem;
+    background-color: #efefef;
+  }
+}
+.landscape-tip {
+  // ++++++++++++++++++++++++++++++++++++++++
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: #fff;
+  display: none;
+  justify-content: center;
+  align-items: center;
+}
+
+// ++++++++++++++++++++++++++++++++++++++++
+@media only screen and (orientation: landscape) {
+  // 横屏
+  .landscape-tip {
+    display: flex;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue"
+
+import App from './App.vue'
+
+// 导入重置样式表
+import './assets/main.scss'
+
+const app = createApp(App)
+
+app.mount('#app')
